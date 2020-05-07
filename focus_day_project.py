c0311aa623cf7e943aca36b9494c620be1179e2b
EmmaArnold <23earnold@athenian.org>
2020-05-07 18:42:49 +0000

    did more

--- focus_day_project.py
+++ focus_day_project.py
@@ -1,2 +1,26 @@
-# Welcome to your Focus Day Project. Replace this comment with something that introduces the user to your project. Be sure to mention the Focus Day and your initials and graduation year. (ie This game is for Pool Volume Day and is written by ML '23.)
-# Also, be sure to use comments throughout your program. Use good programming practices, including organization, documentation and citation. Yes, you need to cite your sources! (You can do so using comments at the bottom of your code.)
+# This game is for epidemic day and is written by EA '23
+def x():
+def a():
+def b():
+def m():
+def r():
+x = 0
+a = 0
+b = 0
+m = 0
+r = 0
+
+if spread >= 10:
+    print(input("you lost"))
+    break
+# x is the difficulty level, a is the spread of the disease, b is the cure, m is the amount of money they have, and r is the reasearch
+x = input(str("Hello and welcome to my game. There is a deadly disease spreading all throughout the world, and it is your job to stop it. You will be asked various questions, and you will answer them based on your knowledge. Please enter your difficulty level. 1 is easy, 2 is medium, 3 is hard:  "))
+if x == 1: 
+    b = b + 2 and r = r + 4 and m = m + 15
+elif x == 2:
+    a = a + 1 and b = b + 1 and r = r + 2 and m = m + 12
+elif x == 3: 
+    a = a + 2 and m = m + 10
+else:
+    x = input(str("Please enter either 1, 2, or 3: "))
+
