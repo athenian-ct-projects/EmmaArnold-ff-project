f425d85ee95b972858688d5abe7e0b0775cf1aa9
EmmaArnold <23earnold@athenian.org>
2020-05-19 16:44:47 +0000

    i did more

--- focus_day_project.py
+++ focus_day_project.py
@@ -24,16 +24,18 @@ a = 0
 b = 0
 m = 0
 r = 0
+h = 0
 first = 0
 second = 0 
 third = 0 
 fourth = 0
-
+if r >= 5: 
+    print("Through your reasearch, we have been able to learn a bit more about this disease. It is a very contagious disease, however it is not really deadly.")
 if a >= 7:      #when disease spread gets too high, the game ends
     print("You lost. The disease spread across the entire world and everyone died. ")   
     break
 # x is the difficulty level, a is the spread of the disease, b is the cure, m is the amount of money they have, and r is the reasearch
-x = input(str("Hello and welcome to my game. There is a deadly disease spreading all throughout the world, and it is your job to stop it. You will be asked various questions, and you will answer them based on your knowledge. Please enter your difficulty level. 1 is easy, 2 is medium, 3 is hard:  "))
+x = print(input(int("Hello and welcome to my game. There is a deadly disease spreading all throughout the world, and it is your job to stop it. You will be asked various questions, and you will answer them based on your knowledge. Please enter your difficulty level. 1 is easy, 2 is medium, 3 is hard:  ")))
 if x == 1: 
     b = b + 2 and r = r + 4 and m = m + 15    #more money and cure progress and less disease spread
 elif x == 2:
@@ -44,7 +46,7 @@ else:
     print("please acutually enter a number from 1 to 3")    #if they dont enter a number from 1 to 3 the game ends
     break 
 
-first = input(int("What do you want to do next? Buy a fancy car(1) or invest some money in reasearch(2):  "))   
+first = print(input(int("What do you want to do next? Buy a fancy car(1) or invest some money in reasearch(2):  "))   
 if first == 1:
     a = a + 2 and m = m - 5    #spread goes up, money goes way down 
 elif first == 2: 
@@ -53,7 +55,7 @@ else:
     print ("next time, please enter either 1 or 2 please")      #if number is not 1 or 2, the game ends(maybe try to find another way to do this)
     break 
 
-second = input(int("Next, do you want to invest more money in reasearch(1) or hold an international hugging day(2): "))
+second = print(input(int("Next, do you want to invest more money in reasearch(1) or hold an international hugging day(2): "))
 if second == 1:
     r = r + 5 and a = a + 1 and m = m - 3 and b = b + 1    
 elif second == 2: 
@@ -61,22 +63,28 @@ elif second == 2:
 else: 
     print ("next time, please enter either 1 or 2 please")
     break 
-third = input(int("Next, do you want to cancel all flights(1) or fly to mars to escape the disease(2): "))
+third = print(input(int("Next, do you want to cancel all flights(1) or fly to mars to escape the disease(2): "))
 if third == 1:
     a = a - 1        # the disease spread is going down
 elif third == 2:      #if this one is chosen, game ends
     print("Your goal was to save the world not yourself! Without you there to help save the world, the world falls into anarchy and everyone dies.")
     break 
-fourth = input(int("Next, do you want to get a sample of the disease to help further develop a cure(1) or do you want to cancel all country to country transportation(2): "))
+fourth = print(input(int("Next, do you want to get a sample of the disease to help further develop a cure(1) or do you want to cancel all country to country transportation(2): "))
 if fourth == 1:        #first question that requires some brain power
     b = b + 2           
-if fourth == 2:         # sample of disease helps cure go up, canceling transportation helps spread go down
+elif fourth == 2:         # sample of disease helps cure go up, canceling transportation helps spread go down
     a = a - 1           # 1 is to try and end the game quickly, 2 is a slow approach
-fifth = input(int("You are doing well so far, there is still a chance to save the world. However things are only going to get harder from here. Now do you want to take a slow approach to this situation and invest some time and money in trying and find a cure for the disease(1) or do you want to slow down the spread of the disease by quarentining everyone(2):  "))
+fifth = print(input(int("You are doing well so far, there is still a chance to save the world. However things are only going to get harder from here. Now do you want to take a slow approach to this situation and invest some time and money in trying and find a cure for the disease(1) or do you want to slow down the spread of the disease by quarentining everyone(2):  "))
 if fifth == 1: 
     b = b + 3 and a = a + 2 and m = m - 5
-if fifth == 2:
+elif fifth == 2:
+    h = h - 5 
     print("You have slowed down the spread of the disease a lot, however the civilians are not pleased with you, so please take this into account when making further decisions")
+    sixth = print(input(int("A lot of people aren't able to feed their families now because they aren't able to go to work now. Do you want to give people stimulus checks(1) or stop the quarentine and just tell people to be more careful and weary:  ")))
+    if sixth == 1:
+        m = m - 10
+        seventh = print(input(int()))
+    if sixth == 2: 
+        a = a + 2
 
     
-
